74a79c6b146d85fff9752f0ea92f366f3b08605c
chenchao <1044316902@qq.com>
2019-12-23 16:58:29 +0800

    Initial commit

---
+++ src/main/java/cn/cc/ccboot/CcbootApplication.java
@@ -0,0 +1,13 @@
+package cn.cc.ccboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cc/ccboot/CcbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cc.ccboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
